8c87c9e073eddff99a16434efc45641e5fca65aa
alex11920 <av61799@gmail.com>
2024-12-12 17:43:48 +0600

    pop

---
+++ src/components/PopularItems.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import SectionTitle from "../shared/SectionTitle";
+
+const PopularItems = () => {
+  return (
+    <div>
+      <SectionTitle
+        heading={"FROM OUR MENU"}
+        subHeading={"---Check it out---"}
+      />
+    </div>
+  );
+};
+
+export default PopularItems;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import Banner from "../components/Banner";
 import Category from "../components/Category";
+import PopularItems from "../components/PopularItems";
 
 const Home = () => {
   return (
     <div className="">
       <Banner />
-      <div className="my-16">
+      <div className="my-12">
         <Category />
+        <PopularItems />
       </div>
     </div>
   );
